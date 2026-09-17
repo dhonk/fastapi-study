1b54c6fcafbdbdb5894d1fc649f47da163c8ad40
suneung-perfect-score <ddanielhongg@gmail.com>
2026-09-16 19:10:40 -0700

    finished lesson 14 - password reset

--- populate_db.py
+++ populate_db.py
@@ -17,7 +17,6 @@ USERS = [
         "username": "CoreyMSchafer",
         "email": "CoreyMSchafer@gmail.com",
         "password": "TestPassword1!",
-        "image": "chaewon.jpg",
     },
     {
         "username": "DefaultDude",
@@ -239,6 +238,7 @@ async def clear_existing_data() -> None:
 
     # Clear database tables (order respects foreign keys)
     async with AsyncSessionLocal() as db:
+        await db.execute(delete(models.PasswordResetToken))
         await db.execute(delete(models.Post))
         await db.execute(delete(models.User))
         await db.commit()
